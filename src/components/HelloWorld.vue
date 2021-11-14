182af43c45fe8664dd260c632b00fdbb2833846f
Afonja Tejumade <tmafonja1@gmail.com>
2021-11-14 20:47:07 +0100

    Update HelloWorld.vue

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -13,87 +13,11 @@
         <h2 class="text-xl mt-4 text-white">
           Tri-ai organisation is an NGO committed to revolutionising the
           training, research and innovation in artificial intelligence in
-          Africa. We have set out plans and agenda to achieve this.
+          Africa. 
         </h2>
-        <p class="text-xl mt-4 text-yellow-600">
-          Stay tuned or reach out to us via the contact form
-        </p>
+       
       </div>
-      <div class="bg-gray-900 w-full lg:w-1/2 xl:w-1/3 py-2 container mx-auto">
-        <p class="text-white" v-if="submitData === true">Message sent!</p>
-        <p class="text-gray-400 px-2" v-else>
-          <a href="https://aisaturdayslagos.github.io/" target="_blank" class="text-yellow-600">AISaturdays Lagos</a> is one of the subsidiaries of Tri-ai. If you wish
-          to start a version of AI saturdays in an African city near you, please
-          fill the form below:
-        </p>
-        <form
-          class="px-4 sm:px-8 pt-6 pb-8 text-left"
-          name="submit-to-google-sheet"
-          ref="contactForm"
-          @submit.prevent="saveToGoogleSheet"
-        >
-          <div class="mb-4">
-            <label
-              class="block text-gray-300 text-sm font-bold mb-2"
-              for="username"
-              >What's your name?</label
-            >
-            <input
-              class="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-              v-model="messenger.name"
-              type="text"
-              placeholder="Your name" required disabled
-            />
-          </div>
-          <div class="mb-4">
-            <label
-              class="block text-gray-300 text-sm font-bold mb-2"
-              for="username"
-              >Your location?</label
-            >
-            <input
-              class="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-              v-model="messenger.location"
-              type="text"
-              placeholder="Your location" required disabled
-            />
-          </div>
-          <div class="mb-6">
-            <label
-              class="block text-gray-300 text-sm font-bold mb-2"
-              for="password"
-              >Email, so we get back to you</label
-            >
-            <input
-              class="appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
-              v-model="messenger.email"
-              type="email"
-              placeholder="Your email address" required disabled
-            />
-          </div>
-          <div class="mb-6">
-            <label
-              class="block text-gray-300 text-sm font-bold mb-2"
-              for="password"
-              >Do you have any message for us?</label
-            >
-            <textarea
-              class="appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
-              v-model="messenger.message"
-              placeholder="Your message" disabled
-            ></textarea>
-          </div>
-          <div class="flex items-center justify-between">
-            <button
-              class="bg-yellow-600 text-white font-bold cursor-not-allowed py-2 px-4 rounded focus:outline-none focus:shadow-outline w-full"
-              type="submit" disabled
-            >
-              Send message
-            </button>
-          </div>
-          <div>This form is currently not available. Have an inquiry? Send us an email at <a href="mailto:hello@tri-ai.org" class="text-yellow-600">hello@tri-ai.org</a></div>
-        </form>
-      </div>
+      
     </div>
   </div>
 </template>
